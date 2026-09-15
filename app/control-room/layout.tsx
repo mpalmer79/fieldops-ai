90f3b4cc7d078168a0a705536e8436c07e6f7dc2
Michael Palmer <mpalm2025@gmail.com>
2026-09-14 21:57:01 -0400

    Load nested light surface refinements

--- app/control-room/layout.tsx
+++ app/control-room/layout.tsx
@@ -1,6 +1,7 @@
 import "../control-room-structure.css";
 import "../control-room.css";
 import "../control-room-light.css";
+import "../control-room-light-details.css";
 
 export default function ControlRoomLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return children;
